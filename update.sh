6c6e444df29e43d4bad78b6a3df8ed3a2effbcd6
Sota Yamashita <sota.yamashita@gmail.com>
2015-05-01 00:39:29 +0900

    Creat update.sh

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e "\033[0;32mUpdates...\033[0m"
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
